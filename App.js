602fd74f00ec8fe025052c06ffc8e93b628adb30
Noah <noahsemus@gmail.com>
2020-07-28 12:14:38 -0400

    optimize App.js

--- App.js
+++ App.js
@@ -3,11 +3,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -20,17 +15,5 @@ const App = () => {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 14,
-    color: 'black',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
 
 export default App;
