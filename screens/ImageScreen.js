9bfd5df730cc970fc8327f4945022366fa06ffec
Noah <noahsemus@gmail.com>
2020-08-17 13:35:52 -0400

    Update ImageScreen.js

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -6,8 +6,8 @@ import {
     StatusBar
 } from 'react-native';
 import { PinchGestureHandler, State } from 'react-native-gesture-handler';
-import Animated, { Value } from 'react-native-reanimated';
-import { onGestureEvent, useVector } from 'react-native-redash';
+import Animated, { Value, useCode, block, cond, eq, } from 'react-native-reanimated';
+import { onGestureEvent, useVector, vec, transformOrigin, timing, spring } from 'react-native-redash';
 
 const AnimatedFastImage = Animated.createAnimatedComponent(FastImage);
 
@@ -15,14 +15,29 @@ const ImageScreen = ({ route, navigation }) => {
     const { item } = route.params;
 
     const state = new Value(State.UNDETERMINED);
-    const scale = new Value(1);
+    const gestureScale = new Value(1);
     const focal = useVector(0, 0)
+    const origin = useVector(0, 0)
     const gestureHandler = onGestureEvent({
         state,
-        scale,
+        scale: gestureScale,
         focalX: focal.x,
         focalY: focal.y
     })
+    const scale = cond(
+        eq(state, State.END),
+        spring({
+            from: gestureScale, to: 1, velocity: 1, config: {
+                damping: 18,
+                mass: 1,
+                stiffness: 150,
+            }
+        }),
+        gestureScale
+    )
+    useCode(() => block([
+        cond(eq(state, State.BEGAN), vec.set(origin, focal))
+    ]), [focal, origin, state])
     return (
         <View style={styles.container}>
             <StatusBar translucent animated backgroundColor="transparent" barStyle='light-content' />
@@ -32,9 +47,12 @@ const ImageScreen = ({ route, navigation }) => {
                     height: '100%'
                 }}>
                     <AnimatedFastImage
-                        style={[styles.Image, {
-                            transform: [{ scale }]
-                        }]}
+                        style={[
+                            styles.Image,
+                            {
+                                transform: [...transformOrigin(origin, { scale })]
+                            }
+                        ]}
                         source={{
                             uri: item.image
                         }}
