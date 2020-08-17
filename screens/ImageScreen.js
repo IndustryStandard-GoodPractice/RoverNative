d87aefaf8dddd95aaf87c12f513a06bb3762e44f
Noah <noahsemus@gmail.com>
2020-08-17 14:26:59 -0400

    fixed pinch to zoom android bug

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -3,41 +3,61 @@ import FastImage from 'react-native-fast-image';
 import {
     View,
     StyleSheet,
-    StatusBar
+    StatusBar,
+    Dimensions
 } from 'react-native';
 import { PinchGestureHandler, State } from 'react-native-gesture-handler';
-import Animated, { Value, useCode, block, cond, eq, } from 'react-native-reanimated';
-import { onGestureEvent, useVector, vec, transformOrigin, timing, spring } from 'react-native-redash';
+import Animated, { Value, useCode, block, cond, eq, set, } from 'react-native-reanimated';
+import { onGestureEvent, useVector, vec, transformOrigin, timing, spring, translate, pinchActive, pinchBegan } from 'react-native-redash';
 
 const AnimatedFastImage = Animated.createAnimatedComponent(FastImage);
+const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
 
 const ImageScreen = ({ route, navigation }) => {
     const { item } = route.params;
 
     const state = new Value(State.UNDETERMINED);
-    const gestureScale = new Value(1);
-    const focal = useVector(0, 0)
-    const origin = useVector(0, 0)
+    const scale = new Value(1);
+    const numberOfPointers = new Value(0);
+    const focal = vec.createValue(0, 0)
+    const origin = vec.createValue(0, 0)
+    const translation = vec.createValue(0, 0)
     const gestureHandler = onGestureEvent({
+        numberOfPointers,
         state,
-        scale: gestureScale,
+        scale,
         focalX: focal.x,
         focalY: focal.y
-    })
-    const scale = cond(
-        eq(state, State.END),
-        spring({
-            from: gestureScale, to: 1, velocity: 1, config: {
-                damping: 18,
-                mass: 1,
-                stiffness: 150,
-            }
-        }),
-        gestureScale
-    )
+    });
+    const adjustedFocal = vec.add({ x: -width / 2, y: -height / 2 }, focal)
     useCode(() => block([
-        cond(eq(state, State.BEGAN), vec.set(origin, focal))
-    ]), [focal, origin, state])
+        cond(pinchBegan(state), vec.set(origin, adjustedFocal)),
+        cond(pinchActive(state, numberOfPointers), vec.set(translation, vec.minus(vec.sub(origin, adjustedFocal)))),
+        cond(eq(state, State.END), [
+            set(translation.x, spring({
+                from: translation.x, to: 0, config: {
+                    damping: 18,
+                    mass: 1,
+                    stiffness: 150,
+                }
+            })),
+            set(translation.y, spring({
+                from: translation.y, to: 0, config: {
+                    damping: 18,
+                    mass: 1,
+                    stiffness: 150,
+                }
+            })),
+            set(scale, spring({
+                from: scale, to: 1, config: {
+                    damping: 18,
+                    mass: 1,
+                    stiffness: 150,
+                }
+            })),
+        ])
+    ]), [origin, state, scale, translation, adjustedFocal, numberOfPointers])
     return (
         <View style={styles.container}>
             <StatusBar translucent animated backgroundColor="transparent" barStyle='light-content' />
@@ -50,7 +70,10 @@ const ImageScreen = ({ route, navigation }) => {
                         style={[
                             styles.Image,
                             {
-                                transform: [...transformOrigin(origin, { scale })]
+                                transform: [
+                                    ...transformOrigin(origin, { scale }),
+                                    ...translate(translation)
+                                ]
                             }
                         ]}
                         source={{
